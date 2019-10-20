871d6f89ff6361474c6a1d92398b39d4e71dbaa2
shagungarg <shagungarg2010@gmail.com>
2019-10-20 17:34:58 +0530

    user signup api

---
+++ src/controllers/UserController.ts
@@ -0,0 +1,26 @@
+import {validationResult} from 'express-validator';
+import User from '../models/User';
+
+export class UserController {
+    static signUp(req, res, next) {
+        const error = validationResult(req);
+        const email = req.body.email;
+        const password = req.body.password;
+        const username = req.body.username;
+        if (!error.isEmpty()) {
+            next(new Error(error.array()[0].msg));
+            return;
+        }
+        const data = {
+            email: email,
+            password: password,
+            username: username
+        };
+        let user = new User(data);
+        user.save().then((user) => {
+            res.send(user);
+        }).catch(err => {
+            next(err);
+        })
+    }
+}
---
+++ src/validators/UserValidators.ts
@@ -0,0 +1,21 @@
+import {body} from 'express-validator';
+import User from '../models/User';
+
+export class UserValidators {
+    static signUp() {
+        return [body('email', 'Email is Required').isEmail().custom((email, {req}) => {
+            return User.findOne({email: email}).then(user => {
+                if (user) {
+                    throw new Error('User Already Exist');
+                } else {
+                    return true;
+                }
+            })
+        }),
+            body('password', 'Password is Required').isAlphanumeric()
+                .isLength({min: 8, max: 20}).withMessage('Password can be from 8-20 Characters only'),
+            body('username', 'Username is Required').isString()];
+    }
+}
+
+
